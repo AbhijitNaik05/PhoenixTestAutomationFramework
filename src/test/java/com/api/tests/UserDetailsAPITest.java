48ad491a50a18228ce0c2f417d8d58f0152227b8
Abhijit Naik <naikabhijit9005@gmail.com>
2025-09-28 13:25:19 +0530

    Created SpecUtil class to handle the common api request config and common assertion.

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -1,38 +1,23 @@
 package com.api.tests;
 
-import static com.api.utils.AuthTokenProvider.getToken;
-import static com.api.utils.ConfigManager.getProperty;
+import static com.api.constant.Role.FD;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.lessThan;
 
 import org.testng.annotations.Test;
 
-import static com.api.constant.Role.*;
+import com.api.utils.SpecUtil;
 
-import io.restassured.http.ContentType;
-import io.restassured.http.Header;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class UserDetailsAPITest {
 	@Test
 	public void userdetailsAPITest()  {
-		Header authHeader = new Header("Authorization",getToken(FD));
 		given()
-		.baseUri(getProperty("BASE_URI"))
-		.and()
-		.header(authHeader)
-		.and()
-		.contentType(ContentType.JSON)
-		.log().uri()
-		.log().method()
-		.log().headers()
-		.log().body()
+		.spec(SpecUtil.requestSpecwithAuth(FD))
 		.when()
 		.get("userdetails")
 		.then()
-		.log().all()
-		.statusCode(200)
-		.time(lessThan(2000L))
+		.spec(SpecUtil.responseSpec_ok())
 		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response_schema/UserDetailsResponseSchema.json"));
 	}
 }
---
+++ src/test/java/com/api/utils/SpecUtil.java
@@ -0,0 +1,60 @@
+package com.api.utils;
+
+import static com.api.utils.ConfigManager.getProperty;
+
+import org.hamcrest.Matchers;
+
+import com.api.constant.Role;
+import com.api.pojo.UserCredentials;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class SpecUtil {
+
+	public static RequestSpecification requestSpec() {
+		RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(getProperty("BASE_URI"))
+				.setContentType(ContentType.JSON).setAccept(ContentType.JSON).log(LogDetail.URI).log(LogDetail.METHOD)
+				.log(LogDetail.HEADERS).log(LogDetail.BODY).build();
+
+		return requestSpecification;
+	}
+
+	public static RequestSpecification requestSpec(Object payload) {
+		RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(getProperty("BASE_URI"))
+				.setContentType(ContentType.JSON).setAccept(ContentType.JSON).setBody(payload)
+				.log(LogDetail.URI).log(LogDetail.METHOD).log(LogDetail.HEADERS).log(LogDetail.BODY).build();
+
+		return requestSpecification;
+	}
+
+	public static RequestSpecification requestSpecwithAuth(Role role) {
+		RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(getProperty("BASE_URI"))
+				.addHeader("Authorization", AuthTokenProvider.getToken(role)).setContentType(ContentType.JSON)
+				.setAccept(ContentType.JSON).log(LogDetail.URI).log(LogDetail.METHOD).log(LogDetail.HEADERS)
+				.log(LogDetail.BODY).build();
+		return requestSpecification;
+	}
+
+	public static ResponseSpecification responseSpec_ok() {
+		ResponseSpecification responseSpecification = new ResponseSpecBuilder().expectContentType(ContentType.JSON)
+				.expectStatusCode(200).expectResponseTime(Matchers.lessThan(1000L)).log(LogDetail.ALL).build();
+		return responseSpecification;
+	}
+	
+	public static ResponseSpecification responseSpec_JSON(int StatusCode) {
+		ResponseSpecification responseSpecification = new ResponseSpecBuilder().expectContentType(ContentType.JSON)
+				.expectStatusCode(StatusCode).expectResponseTime(Matchers.lessThan(1000L)).log(LogDetail.ALL).build();
+		return responseSpecification;
+	}
+	
+	public static ResponseSpecification responseSpec_TEXT(int StatusCode) {
+		ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+				.expectStatusCode(StatusCode).expectResponseTime(Matchers.lessThan(1000L)).log(LogDetail.ALL).build();
+		return responseSpecification;
+	}
+}
