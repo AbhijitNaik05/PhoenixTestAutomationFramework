66f100aef7ad17796c7c8d691f713e6ab2103e01
Abhijit Naik <naikabhijit9005@gmail.com>
2025-10-01 09:36:16 +0530

    Updated the java version to 16. Used Record feature of java and converted all pojo classes to record to reduce the boiler plate code.

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -1,6 +1,12 @@
 package com.api.tests;
 
 import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.testng.annotations.Test;
 import com.api.constant.Role;
 import com.api.pojo.CreateJobPayload;
@@ -10,6 +16,8 @@ import com.api.pojo.CustomerProduct;
 import com.api.pojo.Problems;
 import com.api.utils.SpecUtil;
 
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
 public class CreateJobAPITest {
 
 	@Test
@@ -18,20 +26,18 @@ public class CreateJobAPITest {
 		Customer customer = new Customer("Anika", "Naik", "9999999999", "", "anika@gmail.com", "");
 		CustomerAddress customerAddress = new CustomerAddress("605", "Kohinoor", "MG Road", "Phoenix mall", "Pune",
 				"410033", "India", "maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct("2025-06-18T18:30:00.000Z", "10217383225329",
-				"10217383225329", "10217383225329", "2025-06-18T18:30:00.000Z", 1, 1);
+		CustomerProduct customerProduct = new CustomerProduct("2025-06-18T18:30:00.000Z", "60217383225329",
+				"60217383225329", "60217383225329", "2025-06-18T18:30:00.000Z", 1, 1);
 		Problems problems = new Problems(1, "Camera is not working");
-		Problems [] problemArray = new Problems[1];
-		problemArray[0]=problems;
+		List<Problems> problem = new ArrayList<Problems>();
+		problem.add(problems);
 		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct,
-				problemArray);
-		given()
-		.spec(SpecUtil.requestSpecwithAuth(Role.FD, createJobPayload))
-		.and()
-		.when()
-		.post("job/create").then()
-		.spec(SpecUtil.responseSpec_ok());
-
+				problem);
+		given().spec(SpecUtil.requestSpecwithAuth(Role.FD, createJobPayload)).and().when().post("job/create").then()
+				.spec(SpecUtil.responseSpec_ok())
+				.body(matchesJsonSchemaInClasspath("response_schema/CreateJobAPIResponse.json"))
+				.body("message", equalTo("Job created successfully. ")).body("data.mst_service_location_id", equalTo(1))
+				.body("data.job_number", startsWith("JOB_"));
 
 	}
 }
