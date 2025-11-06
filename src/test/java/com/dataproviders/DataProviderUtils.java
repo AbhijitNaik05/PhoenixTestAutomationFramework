acc0b252a0655507d757832bb3fc99522a8c8435
Abhijit Naik <naikabhijit9005@gmail.com>
2025-11-06 23:07:45 +0530

    Created JsonReaderUtility for loginAPI

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 
 import com.api.request.model.CreateJobPayload;
+import com.api.request.model.UserCredentials;
 import com.api.utils.CSVReaderUtility;
 import com.api.utils.CreateJobBeanMapper;
 import com.api.utils.FakerDataGenerator;
+import com.api.utils.JsonReaderUtil;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
@@ -39,5 +41,10 @@ public class DataProviderUtils {
 		int fakerCountInt=Integer.parseInt(fakerCount);
 		Iterator<CreateJobPayload>payloaditertaor=FakerDataGenerator.generateFakeCreateJobData(fakerCountInt);
 		return payloaditertaor;
+	}
+	
+	@DataProvider (name = "LoginAPIJsonDataProvider",parallel = true)
+	public static Iterator<UserCredentials> loginAPIJsonDataProvider() {
+		return JsonReaderUtil.loadJson("testData/demo.json",UserCredentials[].class);
 	}
 }
